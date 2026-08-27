ea4d53c09593307ec51791141ba9bed8288775ad
Hieu160508 <hieumuteki@gmail.com>
2026-08-27 14:25:30 +0700

    session-01: setup env, run JS in browser and node

---
+++ intro.js
@@ -0,0 +1,13 @@
+// 5 phep tinh khac nhau
+const sum = 5 + 3;
+const division = 10 / 3;
+const text = "Hello" + " " + "World";
+const isAdult = 20 >= 18;
+const nothing = null;
+//
+// Log gia tri va kieu du lieu cua tung cai
+console.log(sum, typeof sum);
+console.log(division, typeof division);
+console.log(text, typeof text);
+console.log(isAdult, typeof isAdult);
+console.log(nothing, typeof nothing);
