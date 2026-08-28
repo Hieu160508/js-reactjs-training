b7a8ae1dff3fdffa8d1756011ffc620eaac0c762
Hieu160508 <hieubm@nkk.com.vn>
2026-08-28 17:32:46 +0700

    session-05: destructuring, spread for immutable object updates

---
+++ training-js/session-05/object.js
@@ -0,0 +1,47 @@
+// const user = { name: "Hieu", age: 20, city: "Ha noi" };
+
+// const name1 = user.name;
+// const age1 = user.age;
+// const city1 = user.city;
+
+// const { name, age, city } = user;
+// console.log(name, age, city); 
+
+// const { name: userName } = user;
+// console.log(userName);
+
+// const { country = "Vietnam" } = user;
+
+// const colors = ["red", "green", "blue"];
+// const [firstColor, secondColor, thirdColor] = colors;
+// console.log(firstColor, secondColor, thirdColor);
+
+// const nums = [1, 2, 3, 4, 5];
+// const newNums = [...nums, 6, 7, 8];
+
+// const user2 = { name: "Hieu", age: 20};
+// const updatedUser2 = { ...user2, city: "Ha noi" };
+// console.log(updatedUser2);
+
+// function sum(...numbers) {
+//     return numbers.reduce((total, n) => total + n, 0);
+// }
+// sum(1, 2, 3);
+// console.log(sum(1, 2, 3));
+
+// const { name, age, ...rest } = user2;
+// console.log(rest); 
+// console.log(name, age);
+
+// const original = { name: "Hieu", address: { city: "Ha noi" } };
+// const copy = { ...original };
+
+// copy.name = "HieuThu2";
+// console.log(original.name); 
+
+// copy.address.city = "Sai Gon";
+// console.log(original.address.city);
+
+
+
+
